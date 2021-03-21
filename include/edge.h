4fe523af44367beb7345c5ba6ab92bbce2f14039
everett <mondliwethu.everett.mthunzi@uni-weimar.de>
2021-03-21 17:21:27 +0100

    revision of edge detection based on vanishing depth values

--- include/edge.h
+++ include/edge.h
@@ -2,25 +2,10 @@
 #define EDGE_H
 
 #include <vector>
-
-#include "axis.h"
-#include "io.h"
 #include "point.h"
 
 namespace edge {
 
-const float VANISHING_RANGE = 0.004;
-std::vector<Point> detect(std::vector<Point>& points)
-{
-    std::vector<Point> proposal;
-    Point centroid = Point::centroid(points);
-
-    for (auto& point : points) {
-        if (point.m_z < centroid.m_z + points.size() * VANISHING_RANGE) {
-            proposal.push_back(point);
-        }
-    }
-    return proposal;
-}
+std::vector<Point> detect(std::vector<Point>& points);
 }
 #endif /* EDGE_H */
