15cf472a623299b1cf18c1f3b348dc1db7228c1c
everett <mondliwethu.everett.mthunzi@uni-weimar.de>
2021-03-23 15:27:37 +0100

    reliable range for vanishing edges

--- src/edge.cpp
+++ src/edge.cpp
@@ -1,15 +1,15 @@
 #include "edge.h"
 
-    const float VANISHING_RANGE = 0.004;
-    std::vector<Point> edge::detect(std::vector<Point>& points)
-    {
-        std::vector<Point> proposal;
-        Point centroid = Point::centroid(points);
+const float VANISHING_RANGE = 8;
+std::vector<Point> edge::detect(std::vector<Point>& points)
+{
+    std::vector<Point> proposal;
+    Point centroid = Point::centroid(points);
 
-        for (auto& point : points) {
-            if (point.m_z < centroid.m_z + points.size() * VANISHING_RANGE) {
-                proposal.push_back(point);
-            }
+    for (auto& point : points) {
+        if (point.m_z < centroid.m_z + VANISHING_RANGE) {
+            proposal.push_back(point);
         }
-        return proposal;
     }
+    return proposal;
+}
