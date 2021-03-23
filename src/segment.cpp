55508081d486c41a8ed2072819a5f52692da064c
everett <mondliwethu.everett.mthunzi@uni-weimar.de>
2021-03-23 15:28:45 +0100

    revisiting dbscan

--- src/segment.cpp
+++ src/segment.cpp
@@ -1,6 +1,6 @@
 #include "segment.h"
-#include "logger.h"
 #include "edge.h"
+#include "logger.h"
 #include "outliers.h"
 #include "polygon.h"
 #include "scan.h"
@@ -9,18 +9,19 @@
 std::vector<Point> segment::cut(std::vector<Point>& points)
 {
     Timer timer;
-    /** final segmentation: extrapolating 'vanishing' depth values,  very fast [ robust ]  */
+    /** final segmentation: extrapolating 'vanishing' depth values,  very fast [
+     * robust ]  */
     std::vector<Point> proposal = edge::detect(points);
-
-    /** final segmentation:  jordan curve-based, slow [ unreliable ] */
-    //std::vector<Point> proposal = polygon::fit(points);
-
-    /** final segmentation: dbscan-based, slow [ robust ] */
-    //std::vector<Point> proposal = scan::density(proposal);
 
     /** removing floating points */
     std::vector<Point> denoised = outliers::remove(proposal);
 
+    /** final segmentation: dbscan-based, slow [ robust ] */
+    std::vector<Point> clusters = scan::density(proposal);
+
+    /** final segmentation:  jordan curve-based, slow [ unreliable ] */
+    // std::vector<Point> proposal = polygon::fit(points);
+
     LOG(INFO) << timer.getDuration() << " ms: final segmentation";
-    return denoised;
+    return clusters;
 }
