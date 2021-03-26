aab063efdae87ac77bfbc458802585f52badc893
everett <mondliwethu.everett.mthunzi@uni-weimar.de>
2021-03-26 10:54:44 +0100

    adding function doc

--- include/segment.h
+++ include/segment.h
@@ -5,6 +5,16 @@
 #include <vector>
 
 namespace segment {
+/**
+ * cut
+ *   Identifies and isolates tabletop interaction context segment.
+ *
+ * @param points
+ *   Unorganized 3D point-cloud representation of tabletop environment.
+ *
+ * @retval
+ *    Segment of tabletop interaction context
+ */
 std::vector<Point> cut(std::vector<Point>& points);
 }
 #endif /* SEGMENT_H */
