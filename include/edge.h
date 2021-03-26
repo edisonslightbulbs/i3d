480171e3a34c6cb2209ece50c943bc859a5cb636
everett <mondliwethu.everett.mthunzi@uni-weimar.de>
2021-03-26 10:54:35 +0100

    adding function doc

--- include/edge.h
+++ include/edge.h
@@ -1,11 +1,21 @@
 #ifndef EDGE_H
 #define EDGE_H
 
-#include <vector>
 #include "point.h"
+#include <vector>
 
 namespace edge {
 
+/**
+ * detect
+ *   Detects the edge of a tabletop surface.
+ *
+ * @param  points
+ *   Coarse segment of tabletop interaction context.
+ *
+ * @retval
+ *   Final segment of tabletop interaction context.
+ */
 std::vector<Point> detect(std::vector<Point>& points);
 }
 #endif /* EDGE_H */
