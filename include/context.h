36322998387f2b578eec569ff138b7bc9e8276d1
everett <mondliwethu.everett.mthunzi@uni-weimar.de>
2021-04-17 13:04:48 +0200

    initial implementation oc Context typedef

---
+++ include/context.h
@@ -0,0 +1,34 @@
+#ifndef CONTEXT_H
+#define CONTEXT_H
+
+#include <iostream>
+#include <memory>
+#include <mutex>
+#include <shared_mutex>
+#include <vector>
+
+#include "point.h"
+
+class Context {
+public:
+    std::vector<Point> m_points;
+
+    explicit Context(std::vector<Point>& points)
+        : m_points(points)
+    {
+        std::cout << "hello brave new world " << points.size() << std::endl;
+    }
+
+    Context() = default;
+
+    Context(const Context& other) { m_points = other.m_points; }
+
+    Context& operator=(Context other)
+    {
+        std::swap(m_points, other.m_points);
+        return *this;
+    }
+
+    [[nodiscard]] std::vector<Point> getContext() const { return m_points; }
+};
+#endif /* CONTEXT_H */
