7ab50b978e68d160e1fe2fc6fb39f5e6cc5e5f17
everett <mondliwethu.everett.mthunzi@uni-weimar.de>
2021-05-14 15:43:12 +0200

    non-deviating naming convention

--- src/intact.cpp
+++ src/intact.cpp
@@ -29,147 +29,150 @@
 #define TRACE
 #endif
 
+// thread safe image handlers
+//
+
 // thread safe point-cloud handlers
 //
 int Intact::getNumPoints()
 {
-    std::lock_guard<std::mutex> lck(pcl_mutex);
+    std::lock_guard<std::mutex> lck(m_mutex);
     return m_numPoints;
 }
 
 std::shared_ptr<std::vector<float>> Intact::getRaw()
 {
-    std::lock_guard<std::mutex> lck(pcl_mutex);
+    std::lock_guard<std::mutex> lck(m_mutex);
     return sptr_raw;
 }
 
 std::shared_ptr<std::vector<uint8_t>> Intact::getRawColor()
 {
-    std::lock_guard<std::mutex> lck(pcl_mutex);
+    std::lock_guard<std::mutex> lck(m_mutex);
     return sptr_rawColor;
 }
 
 void Intact::setRaw(const std::vector<float>& pcl)
 {
-    std::lock_guard<std::mutex> lck(pcl_mutex);
+    std::lock_guard<std::mutex> lck(m_mutex);
     *sptr_raw = pcl;
 }
 
 void Intact::setRawColor(const std::vector<uint8_t>& color)
 {
-    std::lock_guard<std::mutex> lck(pcl_mutex);
+    std::lock_guard<std::mutex> lck(m_mutex);
     *sptr_rawColor = color;
 }
 
 std::shared_ptr<std::vector<Point>> Intact::getRawPoints()
 {
-    std::lock_guard<std::mutex> lck(pcl_mutex);
+    std::lock_guard<std::mutex> lck(m_mutex);
     return sptr_rawPoints;
 }
 
 void Intact::setRawPoints(const std::vector<Point>& points)
 {
-    std::lock_guard<std::mutex> lck(pcl_mutex);
+    std::lock_guard<std::mutex> lck(m_mutex);
     *sptr_rawPoints = points;
 }
 
 std::shared_ptr<std::vector<float>> Intact::getSegment()
 {
-    std::lock_guard<std::mutex> lck(pcl_mutex);
+    std::lock_guard<std::mutex> lck(m_mutex);
     return sptr_segment;
 }
 
 std::shared_ptr<std::vector<uint8_t>> Intact::getSegmentColor()
 {
-    std::lock_guard<std::mutex> lck(pcl_mutex);
+    std::lock_guard<std::mutex> lck(m_mutex);
     return sptr_segmentColor;
 }
 
 std::shared_ptr<std::vector<Point>> Intact::getSegmentPoints()
 {
-    std::lock_guard<std::mutex> lck(pcl_mutex);
+    std::lock_guard<std::mutex> lck(m_mutex);
     return sptr_segmentPoints;
 }
 
 void Intact::setSegment(const std::vector<float>& segment)
 {
-    std::lock_guard<std::mutex> lck(pcl_mutex);
+    std::lock_guard<std::mutex> lck(m_mutex);
     *sptr_segment = segment;
 }
 
 void Intact::setSegmentColor(const std::vector<uint8_t>& color)
 {
-    std::lock_guard<std::mutex> lck(pcl_mutex);
+    std::lock_guard<std::mutex> lck(m_mutex);
     *sptr_segmentColor = color;
 }
 
 void Intact::setSegmentPoints(const std::vector<Point>& points)
 {
-    std::lock_guard<std::mutex> lck(pcl_mutex);
+    std::lock_guard<std::mutex> lck(m_mutex);
     *sptr_segmentPoints = points;
 }
 
 std::shared_ptr<std::vector<float>> Intact::getRegion()
 {
-    std::lock_guard<std::mutex> lck(pcl_mutex);
+    std::lock_guard<std::mutex> lck(m_mutex);
     return sptr_region;
 }
 
 std::shared_ptr<std::vector<uint8_t>> Intact::getRegionColor()
 {
-    std::lock_guard<std::mutex> lck(pcl_mutex);
+    std::lock_guard<std::mutex> lck(m_mutex);
     return sptr_regionColor;
 }
 
 void Intact::setRegion(const std::vector<float>& points)
 {
-    std::lock_guard<std::mutex> lck(pcl_mutex);
+    std::lock_guard<std::mutex> lck(m_mutex);
     *sptr_region = points;
 }
 void Intact::setRegionColor(const std::vector<uint8_t>& color)
 {
-    std::lock_guard<std::mutex> lck(pcl_mutex);
+    std::lock_guard<std::mutex> lck(m_mutex);
     *sptr_regionColor = color;
 }
 
 void Intact::setRegionPoints(const std::vector<Point>& points)
 {
-    std::lock_guard<std::mutex> lck(pcl_mutex);
+    std::lock_guard<std::mutex> lck(m_mutex);
     *sptr_regionPoints = points;
 }
 
 std::shared_ptr<std::vector<Point>> Intact::getRegionPoints()
 {
-    std::lock_guard<std::mutex> lck(pcl_mutex);
+    std::lock_guard<std::mutex> lck(m_mutex);
     return sptr_regionPoints;
 }
 
 void Intact::setObject(const std::vector<float>& points)
 {
-    std::lock_guard<std::mutex> lck(pcl_mutex);
+    std::lock_guard<std::mutex> lck(m_mutex);
     *sptr_object = points;
 }
 void Intact::setObjectColor(const std::vector<uint8_t>& color)
 {
-    std::lock_guard<std::mutex> lck(pcl_mutex);
+    std::lock_guard<std::mutex> lck(m_mutex);
     *sptr_objectColor = color;
 }
 
 void Intact::setObjectPoints(const std::vector<Point>& points)
 {
-    std::lock_guard<std::mutex> lck(pcl_mutex);
+    std::lock_guard<std::mutex> lck(m_mutex);
     *sptr_objectPoints = points;
 }
 
 std::shared_ptr<std::vector<float>> Intact::getObject()
 {
-    std::lock_guard<std::mutex> lck(pcl_mutex);
+    std::lock_guard<std::mutex> lck(m_mutex);
     return sptr_object;
 }
 
 std::shared_ptr<std::vector<uint8_t>> Intact::getObjectColor()
 {
-    std::lock_guard<std::mutex> lck(pcl_mutex);
+    std::lock_guard<std::mutex> lck(m_mutex);
     return sptr_objectColor;
 }
 
@@ -177,97 +180,97 @@ std::shared_ptr<std::vector<uint8_t>> Intact::getObjectColor()
 //
 void Intact::raiseSegmentedFlag()
 {
-    std::lock_guard<std::mutex> lck(pcl_mutex);
+    std::lock_guard<std::mutex> lck(m_mutex);
     *sptr_isContextSegmented = true;
 }
 
 void Intact::raiseClusteredFlag()
 {
-    std::lock_guard<std::mutex> lck(pcl_mutex);
+    std::lock_guard<std::mutex> lck(m_mutex);
     *sptr_isContextClustered = true;
 }
 
 void Intact::raiseEpsilonFlag()
 {
-    std::lock_guard<std::mutex> lck(pcl_mutex);
+    std::lock_guard<std::mutex> lck(m_mutex);
     *sptr_isEpsilonComputed = true;
 }
 
 void Intact::raiseRunFlag()
 {
-    std::lock_guard<std::mutex> lck(pcl_mutex);
+    std::lock_guard<std::mutex> lck(m_mutex);
     *sptr_run = true;
 }
 
 void Intact::raiseStopFlag()
 {
-    std::lock_guard<std::mutex> lck(pcl_mutex);
+    std::lock_guard<std::mutex> lck(m_mutex);
     *sptr_stop = true;
 }
 
 void Intact::stop()
 {
-    std::lock_guard<std::mutex> lck(pcl_mutex);
+    std::lock_guard<std::mutex> lck(m_mutex);
     *sptr_run = false;
 }
 
 bool Intact::isRun()
 {
-    std::lock_guard<std::mutex> lck(pcl_mutex);
+    std::lock_guard<std::mutex> lck(m_mutex);
     return *sptr_run;
 }
 
 bool Intact::isStop()
 {
-    std::lock_guard<std::mutex> lck(pcl_mutex);
+    std::lock_guard<std::mutex> lck(m_mutex);
     return *sptr_stop;
 }
 
 void Intact::raiseKinectReadyFlag()
 {
-    std::lock_guard<std::mutex> lck(pcl_mutex);
+    std::lock_guard<std::mutex> lck(m_mutex);
     *sptr_isKinectReady = true;
 }
 
 bool Intact::isKinectReady()
 {
-    std::lock_guard<std::mutex> lck(pcl_mutex);
+    std::lock_guard<std::mutex> lck(m_mutex);
     return *sptr_isKinectReady;
 }
 
 bool Intact::isSegmented()
 {
-    std::lock_guard<std::mutex> lck(pcl_mutex);
+    std::lock_guard<std::mutex> lck(m_mutex);
     return *sptr_isContextSegmented;
 }
 
 bool Intact::isClustered()
 {
-    std::lock_guard<std::mutex> lck(pcl_mutex);
+    std::lock_guard<std::mutex> lck(m_mutex);
     return *sptr_isContextClustered;
 }
 
 bool Intact::isEpsilonComputed()
 {
-    std::lock_guard<std::mutex> lck(pcl_mutex);
+    std::lock_guard<std::mutex> lck(m_mutex);
     return *sptr_isEpsilonComputed;
 }
 
 bool Intact::isCalibrated()
 {
-    std::lock_guard<std::mutex> lck(pcl_mutex);
+    std::lock_guard<std::mutex> lck(m_mutex);
     return *sptr_isCalibrated;
 }
 
 void Intact::raiseCalibratedFlag()
 {
-    std::lock_guard<std::mutex> lck(pcl_mutex);
+    std::lock_guard<std::mutex> lck(m_mutex);
     *sptr_isCalibrated = true;
 }
 
 std::pair<Point, Point> Intact::getSegmentBoundary()
 {
-    std::lock_guard<std::mutex> lck(pcl_mutex);
+    std::lock_guard<std::mutex> lck(m_mutex);
     return m_segmentBoundary;
 }
 
@@ -478,73 +481,89 @@ void Intact::render(std::shared_ptr<Intact>& sptr_intact)
 
 #define DETECT_OBJECTS 1
 void Intact::detectObjects(std::vector<std::string>& classnames,
-    torch::jit::script::Module& module, k4a_image_t k4aImage,
-    std::shared_ptr<Intact>& sptr_intact)
+    torch::jit::script::Module& module, std::shared_ptr<Intact>& sptr_intact,
+    std::shared_ptr<Kinect>& sptr_kinect)
 
 {
 #if DETECT_OBJECTS == 1
-    clock_t start = clock();
-    int w = k4a_image_get_width_pixels(k4aImage);
-    int h = k4a_image_get_height_pixels(k4aImage);
-    uint8_t* imgData = k4a_image_get_buffer(k4aImage);
-
-    cv::Mat img;
-    cv::Mat frame
-        = cv::Mat(h, w, CV_8UC4, (void*)imgData, cv::Mat::AUTO_STEP).clone();
-    // cv::cvtColor(frame, frame, cv::COLOR_BGRA2BGR);
-
-    /** prepare tensor input */
-    cv::resize(frame, img, cv::Size(640, 384));
-    cv::cvtColor(img, img, cv::COLOR_BGR2RGB);
-    torch::Tensor imgTensor
-        = torch::from_blob(img.data, { img.rows, img.cols, 3 }, torch::kByte);
-    imgTensor = imgTensor.permute({ 2, 0, 1 });
-    imgTensor = imgTensor.toType(torch::kFloat);
-    imgTensor = imgTensor.div(255);
-    imgTensor = imgTensor.unsqueeze(0);
-
-    // preds: [?, 15120, 9]
-    torch::Tensor preds
-        = module.forward({ imgTensor }).toTuple()->elements()[0].toTensor();
-    std::vector<torch::Tensor> dets
-        = yolo::non_max_suppression(preds, 0.4, 0.5);
-
-    if (!dets.empty()) {
-        /** show result */
-        for (size_t i = 0; i < dets[0].sizes()[0]; ++i) {
-            float left
-                = dets[0][i][0].item().toFloat() * (float)frame.cols / 640;
-            float top
-                = dets[0][i][1].item().toFloat() * (float)frame.rows / 384;
-            float right
-                = dets[0][i][2].item().toFloat() * (float)frame.cols / 640;
-            float bottom
-                = dets[0][i][3].item().toFloat() * (float)frame.rows / 384;
-            float score = dets[0][i][4].item().toFloat();
-            int classID = dets[0][i][5].item().toInt();
-
-            cv::rectangle(frame,
-                cv::Rect(left, top, (right - left), (bottom - top)),
-                cv::Scalar(0, 255, 0), 2);
-
-            cv::putText(frame,
-                classnames[classID] + ": " + cv::format("%.2f", score),
-                cv::Point(left, top), cv::FONT_HERSHEY_SIMPLEX,
-                (right - left) / 200, cv::Scalar(0, 255, 0), 2);
-        }
+    while (!sptr_intact->isKinectReady()) {
+        std::this_thread::sleep_for(std::chrono::milliseconds(3));
     }
+    while (sptr_intact->isKinectReady()) {
+
+        if (sptr_kinect->getRgbImage() == nullptr) {
+            continue;
+        }
+        clock_t start = clock();
+        int w = k4a_image_get_width_pixels(sptr_kinect->getRgbImage());
+        int h = k4a_image_get_height_pixels(sptr_kinect->getRgbImage());
+        uint8_t* imgData = k4a_image_get_buffer(sptr_kinect->getRgbImage());
+
+        {
+            std::lock_guard<std::mutex> lck(sptr_kinect->m_mutex);
+            if (sptr_kinect->m_rgbImageCopy != nullptr) {
+                k4a_image_release(sptr_kinect->m_rgbImageCopy);
+            }
+        }
+
+        cv::Mat img;
+        cv::Mat frame
+            = cv::Mat(h, w, CV_8UC4, (void*)imgData, cv::Mat::AUTO_STEP)
+                  .clone();
+
+        /** prepare tensor input */
+        cv::resize(frame, img, cv::Size(640, 384));
+        cv::cvtColor(img, img, cv::COLOR_BGR2RGB);
+        torch::Tensor imgTensor = torch::from_blob(
+            img.data, { img.rows, img.cols, 3 }, torch::kByte);
+        imgTensor = imgTensor.permute({ 2, 0, 1 });
+        imgTensor = imgTensor.toType(torch::kFloat);
+        imgTensor = imgTensor.div(255);
+        imgTensor = imgTensor.unsqueeze(0);
+
+        // preds: [?, 15120, 9]
+        torch::Tensor preds
+            = module.forward({ imgTensor }).toTuple()->elements()[0].toTensor();
+        std::vector<torch::Tensor> dets
+            = yolo::non_max_suppression(preds, 0.4, 0.5);
+
+        if (!dets.empty()) {
+            /** show result */
+            for (size_t i = 0; i < dets[0].sizes()[0]; ++i) {
+                float left
+                    = dets[0][i][0].item().toFloat() * (float)frame.cols / 640;
+                float top
+                    = dets[0][i][1].item().toFloat() * (float)frame.rows / 384;
+                float right
+                    = dets[0][i][2].item().toFloat() * (float)frame.cols / 640;
+                float bottom
+                    = dets[0][i][3].item().toFloat() * (float)frame.rows / 384;
+                float score = dets[0][i][4].item().toFloat();
+                int classID = dets[0][i][5].item().toInt();
+
+                cv::rectangle(frame,
+                    cv::Rect(left, top, (right - left), (bottom - top)),
+                    cv::Scalar(0, 255, 0), 2);
+
+                cv::putText(frame,
+                    classnames[classID] + ": " + cv::format("%.2f", score),
+                    cv::Point(left, top), cv::FONT_HERSHEY_SIMPLEX,
+                    (right - left) / 200, cv::Scalar(0, 255, 0), 2);
+            }
+        }
 
 #define SHOW_DETECTED_OBJECTS 1
 #if SHOW_DETECTED_OBJECTS == 1
-    cv::putText(frame,
-        "FPS: " + std::to_string(int(1e7 / (double)(clock() - start))),
-        cv::Point(50, 50), cv::FONT_HERSHEY_SIMPLEX, 1, cv::Scalar(0, 255, 0),
-        2);
-    cv::imshow("", frame);
+        cv::putText(frame,
+            "FPS: " + std::to_string(int(1e7 / (double)(clock() - start))),
+            cv::Point(50, 50), cv::FONT_HERSHEY_SIMPLEX, 1,
+            cv::Scalar(0, 255, 0), 2);
+        cv::imshow("", frame);
 #endif
 
-    if (cv::waitKey(1) == 27) {
-        sptr_intact->raiseStopFlag();
+        if (cv::waitKey(1) == 27) {
+            sptr_intact->raiseStopFlag();
+        }
     }
 #endif
 }
