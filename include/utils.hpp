b75cf74743b3cbb856829d0449ad556d2153b24d
everett <mondliwethu.everett.mthunzi@uni-weimar.de>
2021-05-22 20:28:45 +0200

    cleaning code base

--- include/utils.hpp
+++ include/utils.hpp
@@ -1,31 +1,21 @@
 #ifndef INTACT_UTILS_H
 #define INTACT_UTILS_H
 
-void chromaPixel(const int& index, uint8_t* ptr_data)
+void stitch(const int& index, Point& point, int16_t* ptr_pcl,
+    uint8_t* ptr_img_GL, uint8_t* ptr_img_CV)
 {
-    ptr_data[4 * index + 0] = 93;  // blue
-    ptr_data[4 * index + 1] = 171; // green
-    ptr_data[4 * index + 2] = 65;  // red
-    ptr_data[4 * index + 3] = 0;   // alpha
+    ptr_pcl[3 * index + 0] = point.m_xyz[0]; // x
+    ptr_pcl[3 * index + 1] = point.m_xyz[1]; // y
+    ptr_pcl[3 * index + 2] = point.m_xyz[2]; // z
+
+    ptr_img_CV[4 * index + 0] = point.m_bgra[0]; // blue
+    ptr_img_CV[4 * index + 1] = point.m_bgra[1]; // green
+    ptr_img_CV[4 * index + 2] = point.m_bgra[2]; // red
+    ptr_img_CV[4 * index + 3] = point.m_bgra[3]; // alpha
+
+    ptr_img_GL[3 * index + 0] = point.m_rgb[0]; // blue
+    ptr_img_GL[3 * index + 1] = point.m_rgb[1]; // green
+    ptr_img_GL[3 * index + 2] = point.m_rgb[2]; // red
 }
 
-bool vacant(
-    const int& index, const short* ptr_data, const Point& min, const Point& max)
-{
-    if (max.m_xyz[2] == (int16_t)INT_MAX || min.m_xyz[2] == (int16_t)INT_MIN) {
-        return false;
-    }
-    if ((int16_t)ptr_data[3 * index + 0] > max.m_xyz[0]
-        || (int16_t)ptr_data[3 * index + 0] < min.m_xyz[0]
-        || (int16_t)ptr_data[3 * index + 1] > max.m_xyz[1]
-        || (int16_t)ptr_data[3 * index + 1] < min.m_xyz[1]) {
-        return false;
-    }
-
-    if ((int16_t)ptr_data[3 * index + 2] > max.m_xyz[2] - 20      // bot
-        && (int16_t)ptr_data[3 * index + 2] < max.m_xyz[2] + 3) { // top
-        return true;
-    }
-    return false;
-}
 #endif /*INTACT_UTILS_H*/
