6ae4a6da6618df55a0c32f8529b1f47b236868d2
everett <mondliwethu.everett.mthunzi@uni-weimar.de>
2021-05-27 15:27:57 +0200

    removing unused declerations

--- include/i3d.h
+++ include/i3d.h
@@ -1,6 +1,8 @@
 #ifndef INTACT_UTILS_H
 #define INTACT_UTILS_H
 
+#include <opencv2/core.hpp>
+#include <opencv2/opencv.hpp>
 #include <string>
 #include <thread>
 #include <torch/script.h>
@@ -13,27 +15,25 @@ namespace i3d {
 void configTorch(
     std::vector<std::string>& classNames, torch::jit::script::Module& module);
 
-void adapt(const int& index, Point& point, const int16_t* ptr_pcl,
-    const uint8_t* ptr_img);
-
-bool invalid(const int& index, const int16_t* ptr_pCloudData,
-    const uint8_t* ptr_imgData);
-
-void addPoint(const int& index, int16_t* ptr_pcl);
-
-bool inSegment(const int& index, const short* ptr_pclData,
-    const Point& minPoint, const Point& maxPoint);
-
-void stitch(const int& index, Point& point, int16_t* ptr_pcl,
-    uint8_t* ptr_img_GL, uint8_t* ptr_img_CV);
-
-std::pair<Point, Point> queryBoundary(std::vector<Point>& points);
+void adapt(const int& index, Point& point,
+    const std::vector<int16_t>& pCloudFrame,
+    const std::vector<uint8_t>& imgFrame);
 
 bool invalid(const int& index, const k4a_float2_t* ptr_xyTable,
     const uint16_t* ptr_depth);
 
+bool invalid(const int& index, const int16_t* ptr_pCloudData,
+    const uint8_t* ptr_imgData);
+
+bool null(const int& index, std::vector<int16_t>& pCloudFrame,
+    std::vector<uint8_t>& imgFrame);
+
+void addXYZ(const int& index, std::vector<int16_t>& pCloudFrame);
+
 void addXYZ(const int& index, std::vector<int16_t>& pCloudFrame,
     const int16_t* ptr_pCloudData);
+
+void addPixel_GL(const int& index, std::vector<uint8_t>& imgFrame_GL);
 
 void addPixel_GL(const int& index, std::vector<uint8_t>& imgFrame_GL,
     const uint8_t* ptr_imgData);
@@ -41,26 +41,14 @@ void addPixel_GL(const int& index, std::vector<uint8_t>& imgFrame_GL,
 void addPixel_CV(const int& index, std::vector<uint8_t>& imgFrame_CV,
     const uint8_t* ptr_imgData);
 
-void adapt(const int& index, Point& point,
-    const std::vector<int16_t>& pCloudFrame,
-    const std::vector<uint8_t>& imgFrame);
-
-void addXYZ(const int& index, std::vector<int16_t>& pCloudFrame);
-
-void addPixel_GL(const int& index, std::vector<uint8_t>& imgFrame_GL);
-
-void addPixel_GL(const int& index, std::vector<uint8_t>& imgFrame_GL,
-    const std::vector<uint8_t>& imgFrame);
-
-void addXYZ(const int& index, std::vector<int16_t>& pCloudFrame,
-    const std::vector<int16_t>& otherPCloudFrame);
-
-bool inSegment(const int& index, const std::vector<int16_t>& ptr_pclData,
-    const Point& minPoint, const Point& maxPoint);
-
 void addPixel_CV(const int& index, std::vector<uint8_t>& imgFrame_CV);
 
-bool null(const int& index, std::vector<int16_t>& pCloudFrame,
-    std::vector<uint8_t>& imgFrame);
+bool inSegment(const int& index, const std::vector<int16_t>& pCloudFrame,
+    const Point& minPoint, const Point& maxPoint);
+
+void stitch(const int& index, Point& point, int16_t* ptr_pcl,
+    uint8_t* ptr_img_GL, uint8_t* ptr_img_CV);
+
+std::pair<Point, Point> queryBoundary(std::vector<Point>& points);
 }
 #endif /*INTACT_UTILS_H*/
