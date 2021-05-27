b73df40eb937ab975c0fb245a1aba89ff28b5a18
everett <mondliwethu.everett.mthunzi@uni-weimar.de>
2021-05-27 15:11:46 +0200

    revising global helper functions

--- include/i3d.h
+++ include/i3d.h
@@ -16,29 +16,51 @@ void configTorch(
 void adapt(const int& index, Point& point, const int16_t* ptr_pcl,
     const uint8_t* ptr_img);
 
-bool invalid(const int& index, const int16_t* ptr_pcl, const uint8_t* ptr_img);
+bool invalid(const int& index, const int16_t* ptr_pCloudData,
+    const uint8_t* ptr_imgData);
 
 void addPoint(const int& index, int16_t* ptr_pcl);
 
-void addPoint(
-    const int& index, int16_t* ptr_pclDest, const int16_t* ptr_pclSrc);
-
-void addPixel_CV(
-    const int& index, uint8_t* ptr_imgDest, const uint8_t* ptr_imgSrc);
-
-void addPixel_GL(
-    const int& index, uint8_t* ptr_imgDest, const uint8_t* ptr_imgSrc);
-
-void addPixel_CV(const int& index, uint8_t* ptr_img);
-
-void addPixel_GL(const int& index, uint8_t* ptr_img);
-
-bool inSegment(
-    const int& index, const short* ptr_pcl, const Point& min, const Point& max);
+bool inSegment(const int& index, const short* ptr_pclData,
+    const Point& minPoint, const Point& maxPoint);
 
 void stitch(const int& index, Point& point, int16_t* ptr_pcl,
     uint8_t* ptr_img_GL, uint8_t* ptr_img_CV);
 
 std::pair<Point, Point> queryBoundary(std::vector<Point>& points);
+
+bool invalid(const int& index, const k4a_float2_t* ptr_xyTable,
+    const uint16_t* ptr_depth);
+
+void addXYZ(const int& index, std::vector<int16_t>& pCloudFrame,
+    const int16_t* ptr_pCloudData);
+
+void addPixel_GL(const int& index, std::vector<uint8_t>& imgFrame_GL,
+    const uint8_t* ptr_imgData);
+
+void addPixel_CV(const int& index, std::vector<uint8_t>& imgFrame_CV,
+    const uint8_t* ptr_imgData);
+
+void adapt(const int& index, Point& point,
+    const std::vector<int16_t>& pCloudFrame,
+    const std::vector<uint8_t>& imgFrame);
+
+void addXYZ(const int& index, std::vector<int16_t>& pCloudFrame);
+
+void addPixel_GL(const int& index, std::vector<uint8_t>& imgFrame_GL);
+
+void addPixel_GL(const int& index, std::vector<uint8_t>& imgFrame_GL,
+    const std::vector<uint8_t>& imgFrame);
+
+void addXYZ(const int& index, std::vector<int16_t>& pCloudFrame,
+    const std::vector<int16_t>& otherPCloudFrame);
+
+bool inSegment(const int& index, const std::vector<int16_t>& ptr_pclData,
+    const Point& minPoint, const Point& maxPoint);
+
+void addPixel_CV(const int& index, std::vector<uint8_t>& imgFrame_CV);
+
+bool null(const int& index, std::vector<int16_t>& pCloudFrame,
+    std::vector<uint8_t>& imgFrame);
 }
 #endif /*INTACT_UTILS_H*/
