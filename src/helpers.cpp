f8e7e8126c80b20f16b1f338b25a349bf4f3ace4
everett <mondliwethu.everett.mthunzi@uni-weimar.de>
2021-05-28 12:04:55 +0200

    revising code base

--- src/helpers.cpp
+++ src/helpers.cpp
@@ -1,7 +1,7 @@
 #include "helpers.h"
 #include <cmath>
 
-void i3d::configTorch(
+void utils::configTorch(
     std::vector<std::string>& classNames, torch::jit::script::Module& module)
 {
     const std::string scriptName = io::pwd() + "/resources/torchscript.pt";
@@ -14,7 +14,7 @@ void i3d::configTorch(
     }
 }
 
-void i3d::adapt(const int& index, Point& point,
+void utils::adapt(const int& index, Point& point,
     const std::vector<int16_t>& pCloudFrame,
     const std::vector<uint8_t>& imgFrame)
 {
@@ -37,7 +37,7 @@ void i3d::adapt(const int& index, Point& point,
     point.setPixel_CV(bgra);
 }
 
-bool i3d::null(const int& index, std::vector<int16_t>& pCloudFrame,
+bool utils::null(const int& index, std::vector<int16_t>& pCloudFrame,
     std::vector<uint8_t>& imgFrame)
 {
     if (pCloudFrame[3 * index + 2] == 0 && imgFrame[4 * index + 3] == 0) {
@@ -46,7 +46,7 @@ bool i3d::null(const int& index, std::vector<int16_t>& pCloudFrame,
     return false;
 }
 
-bool i3d::invalid(const int& index, const k4a_float2_t* ptr_xyTable,
+bool utils::invalid(const int& index, const k4a_float2_t* ptr_xyTable,
     const uint16_t* ptr_depth)
 {
     if (ptr_depth[index] == 0 && std::isnan(ptr_xyTable[index].xy.x)
@@ -64,7 +64,7 @@ bool i3d::invalid(const int& index, const k4a_float2_t* ptr_xyTable,
     return false;
 }
 
-bool i3d::invalid(
+bool utils::invalid(
     const int& index, const int16_t* ptr_pCloudData, const uint8_t* ptr_imgData)
 {
     if (ptr_pCloudData[3 * index + 2] == 0
@@ -77,7 +77,7 @@ bool i3d::invalid(
     return false;
 }
 
-void i3d::addXYZ(const int& index, std::vector<int16_t>& pCloudFrame,
+void utils::addXYZ(const int& index, std::vector<int16_t>& pCloudFrame,
     const int16_t* ptr_pCloudData)
 {
     pCloudFrame[3 * index + 0] = ptr_pCloudData[3 * index + 0];
@@ -85,14 +85,14 @@ void i3d::addXYZ(const int& index, std::vector<int16_t>& pCloudFrame,
     pCloudFrame[3 * index + 2] = ptr_pCloudData[3 * index + 2];
 }
 
-void i3d::addXYZ(const int& index, std::vector<int16_t>& pCloudFrame)
+void utils::addXYZ(const int& index, std::vector<int16_t>& pCloudFrame)
 {
     pCloudFrame[3 * index + 0] = 0;
     pCloudFrame[3 * index + 1] = 0;
     pCloudFrame[3 * index + 2] = 0;
 }
 
-void i3d::addPixel_CV(const int& index, std::vector<uint8_t>& imgFrame_CV,
+void utils::addPixel_CV(const int& index, std::vector<uint8_t>& imgFrame_CV,
     const uint8_t* ptr_imgData)
 {
     imgFrame_CV[4 * index + 0] = ptr_imgData[4 * index + 0]; // blue
@@ -101,7 +101,7 @@ void i3d::addPixel_CV(const int& index, std::vector<uint8_t>& imgFrame_CV,
     imgFrame_CV[4 * index + 3] = ptr_imgData[4 * index + 3]; // alpha
 }
 
-void i3d::addPixel_GL(const int& index, std::vector<uint8_t>& imgFrame_GL,
+void utils::addPixel_GL(const int& index, std::vector<uint8_t>& imgFrame_GL,
     const uint8_t* ptr_imgData)
 {
     imgFrame_GL[4 * index + 2] = ptr_imgData[4 * index + 0]; // blue
@@ -110,7 +110,7 @@ void i3d::addPixel_GL(const int& index, std::vector<uint8_t>& imgFrame_GL,
     imgFrame_GL[4 * index + 3] = ptr_imgData[4 * index + 3]; // alpha
 }
 
-void i3d::addPixel_GL(const int& index, std::vector<uint8_t>& imgFrame_GL)
+void utils::addPixel_GL(const int& index, std::vector<uint8_t>& imgFrame_GL)
 {
     imgFrame_GL[4 * index + 0] = 0; // red
     imgFrame_GL[4 * index + 1] = 0; // green
@@ -118,7 +118,7 @@ void i3d::addPixel_GL(const int& index, std::vector<uint8_t>& imgFrame_GL)
     imgFrame_GL[4 * index + 3] = 0; // alpha
 }
 
-void i3d::addPixel_CV(const int& index, std::vector<uint8_t>& imgFrame_CV)
+void utils::addPixel_CV(const int& index, std::vector<uint8_t>& imgFrame_CV)
 {
     imgFrame_CV[4 * index + 0] = 0; // red
     imgFrame_CV[4 * index + 1] = 0; // green
@@ -126,7 +126,7 @@ void i3d::addPixel_CV(const int& index, std::vector<uint8_t>& imgFrame_CV)
     imgFrame_CV[4 * index + 3] = 0; // alpha
 }
 
-bool i3d::inSegment(const int& index, const std::vector<int16_t>& pCloudFrame,
+bool utils::inSegment(const int& index, const std::vector<int16_t>& pCloudFrame,
     const Point& minPoint, const Point& maxPoint)
 {
     if (pCloudFrame[3 * index + 2] == 0) {
@@ -144,7 +144,7 @@ bool i3d::inSegment(const int& index, const std::vector<int16_t>& pCloudFrame,
     return true;
 }
 
-void i3d::stitch(const int& index, Point& point, int16_t* ptr_pcl,
+void utils::stitch(const int& index, Point& point, int16_t* ptr_pcl,
     uint8_t* ptr_img_GL, uint8_t* ptr_img_CV)
 {
     ptr_pcl[3 * index + 0] = point.m_xyz[0]; // x
@@ -162,7 +162,7 @@ void i3d::stitch(const int& index, Point& point, int16_t* ptr_pcl,
     ptr_img_GL[4 * index + 3] = point.m_rgba[3]; // alpha
 }
 
-std::pair<Point, Point> i3d::queryBoundary(std::vector<Point>& points)
+std::pair<Point, Point> utils::queryBoundary(std::vector<Point>& points)
 {
     std::vector<int16_t> X(points.size());
     std::vector<int16_t> Y(points.size());
